d88f5fc78d3fb9340c2fe2d3a37b834d727d520b
Hiroaki Imai <hiro.1114.aki@gmail.com>
2017-11-12 02:58:07 +0900

    componentWillReceivePropsをお試し

--- src/js/appRedux2.jsx
+++ src/js/appRedux2.jsx
@@ -3,6 +3,12 @@ import { render } from 'react-dom';
 
 import Root from './components/root';
 
+/**
+ * Redux チュートリアルサンプル
+ * ざっくり React チュートリアル
+ * https://qiita.com/pullphone/items/efcaee59cf2a5725c61d
+ */
+
 // ReactDOM.render(...) と同じ意味合い
 render(
     <Root title="react redux sample.">
